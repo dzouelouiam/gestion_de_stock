4654830ed78a6d6cd0c5b61aee654228391d5a94
dzouelouiam <dzouelouiammohamed@gmail.com>
2023-07-21 20:26:35 +0100

    get user & protect

--- stage_backend/routes/userRoute.js
+++ stage_backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser,logout } = require("../controllers/userController");
+const { registerUser, loginUser,logout, getUser } = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
-router.get("/getuser", getuser);
+router.get("/getuser",protect,getUser);
 
 module.exports = router;
